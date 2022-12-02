57198e55375386b4c986974112a83a101c4ed081
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-02 05:37:41 +0000

    add: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import  {writeFile} from "fs/promises"
+import * as url from 'url';
+import path from "path"
+const filePath = url.fileURLToPath(new URL('.', import.meta.url));
+
 const create = async () => {
-    // Write your code here 
-};
+            try{
+                await writeFile(
+                    path.join(filePath, 'files', 'fresh.txt'),
+                    'I am fresh and young',
+                    {flag: 'wx'})
+                        console.log('Файл создан');
+                    }catch (err){
+                throw new Error('FS operation failed')
+            }
+}
 
 await create();
