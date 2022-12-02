d25e21d72618b9caf965b498681308f87f4f9491
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-02 07:42:13 +0000

    feat: fs:copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,18 +1,17 @@
 import fs from 'fs';
 import {__dirname} from "./consts.js";
-import { copyFile, mkdir, readdir} from 'fs/promises'
+import {copyFile, mkdir, readdir} from 'fs/promises'
+
 const copy = async () => {
     const files = __dirname + '/files';
     const files_copy = __dirname + '/files_copy';
-    try{
+    try {
         const dir_files = await readdir(files);
         await mkdir(files_copy);
         await Promise.all(dir_files.map(f => copyFile(`${files}/${f}`, `${files_copy}/${f}`)));
         console.log('Файлы скопированы в files_copy')
-    }catch (err){
-        if(fs.existsSync(files_copy)) {
-            throw new Error('FS operation failed')
-        }
+    } catch (err) {
+        throw new Error('FS operation failed')
     }
 };
 
