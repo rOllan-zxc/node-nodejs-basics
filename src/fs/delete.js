67d74d6211f1a668fec52a41c3ce709795565bcf
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-03 19:30:26 +0000

    feat: fs:delete done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import {__dirname} from "./consts.js";
+import {unlink} from "fs/promises"
+
+
 const remove = async () => {
-    // Write your code here 
+    const delFile = __dirname + '/files/fileToRemove.txt';
+    try{
+        await unlink(delFile);
+    } catch (err){
+        throw new Error('FS operation failed')
+    }
 };
 
 await remove();
