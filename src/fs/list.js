5206ebc54070ef3586ccb2539515d404e3ece5ae
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-03 19:40:15 +0000

    feat: fs:list done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import {__dirname} from "./consts.js";
+import {readdir} from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const files = __dirname + '/files';
+    try{
+        const files_arr = [];
+        const dir_files = await readdir(files);
+        await Promise.all(dir_files.map(n=>files_arr.push(`${n}`)))
+        console.log(files_arr)
+    } catch (err){
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();
