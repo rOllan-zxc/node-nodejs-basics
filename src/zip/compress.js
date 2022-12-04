b75d8092bf9a406f7cce26c2a43ad05f05fb2f18
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-04 21:34:58 +0000

    feat: zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import {fileURLToPath} from "url"
+import {dirname} from 'path'
+import {createGzip} from 'zlib'
+import {createReadStream, createWriteStream} from "fs";
+import {pipeline} from "stream";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const compress = async () => {
-    // Write your code here 
+    const file_to_read = __dirname + '/files/fileToCompress.txt'
+    const file_to_write= __dirname + '/files/archive.gz'
+    const gzip = createGzip();
+    const inp = createReadStream(file_to_read)
+    const out = createWriteStream(file_to_write)
+    pipeline(inp, gzip, out, (err) => {
+        if (err){
+            console.error('compress operation failed')
+        }
+    })
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import {fileURLToPath} from "url"
+import {dirname} from 'path'
+import {createGunzip, createGzip} from 'zlib'
+import {createReadStream, createWriteStream} from "fs";
+import {pipeline} from "stream";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const decompress = async () => {
-    // Write your code here 
+    const file_to_write = __dirname + '/files/fileToCompress.txt'
+    const file_to_read = __dirname + '/files/archive.gz'
+
+    const gzip = createGunzip();
+    const inp = createReadStream(file_to_read)
+    const out = createWriteStream(file_to_write)
+    pipeline(inp, gzip, out, (err) => {
+        if (err){
+            console.error('decompress operation failed')
+        }
+    })
 };
 
 await decompress();
