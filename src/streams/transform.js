9757547ac74ce019918feeca96c39a07ab00c8f7
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-04 20:55:29 +0000

    feat: stream:transform done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
+import {Transform, pipeline} from 'stream'
+
 const transform = async () => {
-    // Write your code here 
+    const tr_stream = new Transform({
+        transform(chunk, enc, callback) {
+            callback(null, String(chunk).split('').reverse().join('')+'\n')
+        }
+    })
+    pipeline(process.stdin, tr_stream, process.stdout, err => {throw new Error()})
 };
 
 await transform();
