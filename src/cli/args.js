eea17fd94411b5b0c5d074ab1dc578334c65474f
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-04 04:55:27 +0000

    feat: cli:args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    const args = process.argv.slice(2)
+    let str_res=''
+    for (let arg of args) {
+        if (arg.includes('--')) {
+            str_res += `${arg.slice(2)} is `
+        } else {
+            str_res += `${arg}, `
+        }
+    }
+    console.log(str_res.slice(0, -2))
+}
 
 parseArgs();
