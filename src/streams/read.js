3d441d7cb79e0a23f11a705dff519e4b54932b9c
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-04 19:13:08 +0000

    feat: stream:read done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import {createReadStream} from 'fs'
+import {fileURLToPath} from "url"
+import {dirname} from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const read = async () => {
-    // Write your code here 
+    const file_path = __dirname + '/files/fileToRead.txt'
+    const stream = createReadStream(file_path, {encoding: 'utf-8'})
+    stream.pipe(process.stdout)
 };
 
 await read();
