2e20181fbe9aed5a8a4c8a54b17781eef8209231
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-04 05:47:38 +0000

    feat: hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import {fileURLToPath} from "url"
+import {dirname} from 'path'
+import {createHash} from 'crypto'
+import {readFile} from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url) ;
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file_to_hash = __dirname + '/files/fileToCalculateHashFor.txt'
+    try {
+        const str_to_hash = await readFile(file_to_hash, {encoding:'utf8'})
+        const hash = await createHash('sha3-256').update(str_to_hash).digest('hex');
+        console.log(hash)
+    } catch (err){
+        throw new Error('Hash operation failed')
+    }
 };
 
 await calculateHash();
