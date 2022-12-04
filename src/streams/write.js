9968464709aa90e11b7293306840a84161f6e02d
rOllan-zxc <nikitalnlykianenko0103@gmail.com>
2022-12-04 19:52:21 +0000

    feat: stream:write done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import {createWriteStream} from 'fs'
+import {fileURLToPath} from "url"
+import {dirname} from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const write = async () => {
-    // Write your code here 
+    const file_path = __dirname + '/files/fileToWrite.txt'
+    const stream = createWriteStream(file_path, {encoding: 'utf-8'})
+    process.stdin.on("data", (data) => stream.write(data))
 };
 
 await write();
